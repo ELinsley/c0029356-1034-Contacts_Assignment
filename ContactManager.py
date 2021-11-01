832154d237f5980cbce423e666f024d2ddddc23f
ELinsley <c0029367@newcastle.ac.uk>
2021-11-01 11:47:49 +0000

    Moved the MainMenu function to main.py

--- ContactManager.py
+++ ContactManager.py
@@ -33,45 +33,6 @@ class ContactManager():
             self.contactList.append(Contact.Contact(name, address, phoneNumber, birthday))
             ##print(self.contactList[i].Get_Name()) ##Debug
 
-    def MainMenu(self):
-        """Brings up the main menu UI, then calls other functions based on user"""
-        print("+-------------------------+")
-        print("| 1) Display all contacts |")
-        print("| 2) Search for a contact |")
-        print("| 3) Edit a contact       |")
-        print("| 4) Delete a contact     |")
-        print("| 5) Save changes         |")
-        print("| Enter 'Exit' to save    |")
-        print("| and quit.               |")
-        print("+-------------------------+")
-        print("(1-4 / Exit)")
-
-        validInput = False
-        userInput = input("Enter your selection here: ")
-        while validInput == False:
-            if userInput == "1":
-                print("'Display all contacts' selected...")
-                validInput = True
-                self.DisplayAllContacts()
-            elif userInput == "2":
-                print("'Search for a contact' selected...")
-                validInput = True
-                self.searchContacts()
-            elif userInput == "3":
-                print("'Edit a contact' selected...")
-                validInput = True
-                self.editContact()
-            elif userInput == "4":
-                print("'Delete a contact' selected...")
-                validInput = True
-            elif userInput == "Exit":
-                print("'Exit' selected...")
-                validInput=True
-            else:
-                print("That is not a valid input, please try again...")
-
-        return userInput ##might not need
-
     def DisplayAllContacts(self):
         """Displays all the details of all existing contacts"""
         print("")
--- main.py
+++ main.py
@@ -3,4 +3,39 @@ import ContactManager
 if __name__ == "__main__":
     ##print("Main") ## Debug
     contactManager = ContactManager.ContactManager() ##This also loads all contacts
-    contactManager.MainMenu()
+
+    """Brings up the main menu UI, then calls other functions based on user"""
+    print("+-------------------------+")
+    print("| 1) Display all contacts |")
+    print("| 2) Search for a contact |")
+    print("| 3) Edit a contact       |")
+    print("| 4) Delete a contact     |")
+    print("| 5) Save changes         |")
+    print("| Enter 'Exit' to save    |")
+    print("| and quit.               |")
+    print("+-------------------------+")
+    print("(1-4 / Exit)")
+
+    validInput = False
+    userInput = input("Enter your selection here: ")
+    while validInput == False:
+        if userInput == "1":
+            print("'Display all contacts' selected...")
+            validInput = True
+            contactManager.DisplayAllContacts()
+        elif userInput == "2":
+            print("'Search for a contact' selected...")
+            validInput = True
+            contactManager.searchContacts()
+        elif userInput == "3":
+            print("'Edit a contact' selected...")
+            validInput = True
+            contactManager.editContact()
+        elif userInput == "4":
+            print("'Delete a contact' selected...")
+            validInput = True
+        elif userInput == "Exit":
+            print("'Exit' selected...")
+            validInput=True
+        else:
+            print("That is not a valid input, please try again...")
